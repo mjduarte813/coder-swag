5648398ed8eff8bec5a636a81b9b72921cdd2afc
Marcel <marceljduarte@gmail.com>
2017-08-09 09:52:38 -0600

    Lesson-01: Initial UI with CategoriesVC and CategoryCell View class

---
+++ coder-swag/coder-swag/View/CategoryCell.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Marcel Duarte on 8/9/17.
+//  Copyright © 2017 Marcel Duarte. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
