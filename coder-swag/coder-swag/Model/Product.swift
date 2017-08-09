25248f4067cb24a9f52cfc97e8014c29984e1a4a
Marcel <marceljduarte@gmail.com>
2017-08-09 11:55:43 -0600

    Lesson-04: Added Product data model and updated Data Service class

---
+++ coder-swag/coder-swag/Model/Product.swift
@@ -0,0 +1,21 @@
+//
+//  Product.swift
+//  coder-swag
+//
+//  Created by Marcel Duarte on 8/9/17.
+//  Copyright © 2017 Marcel Duarte. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+    private(set) public var title: String
+    private(set) public var price: String
+    private(set) public var imageName: String
+    
+    init(title: String, price: String, imageName: String) {
+        self.title = title
+        self.price = price
+        self.imageName = imageName
+    }
+}
